3f698ed785e6f94460ccc5c9cc20c24699793b2d
Luigi-bot37 <143562859+Luigi-bot37@users.noreply.github.com>
2024-04-23 19:48:34 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luisin
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x;
+        Scanner captura= new Scanner(System.in);
+        System.out.println("Ingrese un numero:");
+        x= captura.nextInt();
+        System.out.println(x);
+    }
+    
+}
